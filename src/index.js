493206c68f58f4794600d1ff30a404cc8ec98362
Fabíola Augusto <fabiolaccaugusto@gmail.com>
2022-08-02 09:10:05 -0300

    Segundo Componente com parametros

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 
 
 
 ReactDOM.render(
     <div>
-       <Primeiro></Primeiro>
+        <Primeiro></Primeiro>
+        <ComParametro
+            titulo='Situação do Aluno'
+            aluno=' Pedro ' nota={ 9.3}/>
+             <ComParametro
+            titulo='Situação do Aluno'
+            aluno=' Maria ' nota={ 5.8}/>
     </div>,
     document.getElementById('root')
 )
-
-
-
-
