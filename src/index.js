63fb24fc3203837d6fc03090a2ec64db6e0cb1f6
Fabíola Augusto <fabiolaccaugusto@gmail.com>
2022-08-02 07:00:20 -0300

    hello world

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    'Hello World !!!',
+    document.getElementById('root')
+)
+
+
+// A partir dele que vou usar para renderizar algo na tela, 
+// para exibir algo com o método render, 
+// para renderizar e esse método recebe dois paramtros, 
+// o primeiro é o que eu quero renderizar na tela:
+// ReactDOM.render('Hello World!!!');
+
+
+// o segundo parametro vai ser o elemento no qual eu quero inserir o conteúdo 'Hello World!!!' 
+// const el = document.getElementById
+
+//formas diferentes:
+//const el = document.getElementById('root')
+//ReactDOM.render('Hello World !!!', el);
+
+//ReactDOM.render(
+//    'Hello World !!!',
+//    document.getElementById('root')
+//)
+
+// utilizei o react dom para inserir uma string normal de js dentro da página.
